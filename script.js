a11f61cb8ce2fdbffd3d2985bac79936cd70f106
clydx12 <115690125+clydx12@users.noreply.github.com>
2025-05-14 00:43:56 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,17 +5,30 @@ var table = document.getElementById("queue");
 function submit() {
 	var fname = document.getElementById("fname").value;
 	var lname = document.getElementById("lname").value;
-	setCookie(fname + " " + lname + "+");
+	setCookie(fname + " " + lname);
 }
 
 function setCookie(value) {
-	document.cookie = document.cookie + value;
+	const d = new Date();
+	d.setTime(d.getTime());
+	let login = "login=" + d.toUTCString();
+	document.cookie = "name=" + value + ";" + login + ";path=/";
 }
 
 function getCookie() {
-	var x = document.cookie;
-	x.split("+");
-	queue = x;
+	let name = "name=";
+	let decodedCookie = decodeURIComponenet(document.cookie);
+	let ca = decodedCookie.split(';');
+	for (let i = 0; i < ca.length; i++) {
+		let c = ca[i];
+		while (c.charAt(0) == ' ') {
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) == 0) {
+			return c.substring(name.length, c.length);
+		}
+	}
+	return "";
 }
 
 function updateQueue() {
